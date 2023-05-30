1939d890fccacaf3434b6be17d95cc467a859ca9
maxtroezra <maestroezra04@gmail.com>
2023-05-30 20:37:50 +0700

    ubah

--- index.js
+++ index.js
@@ -16,7 +16,3 @@ const server = http.createServer((req, res) => {
     res.end(`The area of a circle with radius ${radius} is ${area}.\n`);
   }
 });
-
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
-});
